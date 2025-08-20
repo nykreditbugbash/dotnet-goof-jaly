54fc888096e4966f90e1829a4a2fd7a1ef282761
Jakob-Lystbak <jaly@nykredit.dk>
2025-08-20 12:27:38 +0200

    Update SQLInjection4.cs

--- bin/Injection/SQLInjection4.cs
+++ bin/Injection/SQLInjection4.cs
@@ -15,7 +15,7 @@ namespace NETStandaloneBlot.Injection
             ServerConnection sc = new ServerConnection();
             sc.ConnectionString = "";
             Server server = new Server(sc);
-            string commandText = "SELECT * FROM users WHERE ( name = '" + System.Console.ReadLine() + "')";
+            string commandText = "SELECT * FROM users WHERE ( name = '" + "System.Console.ReadLine()" + "')";
             // CTSECISSUE: SQLInjection
             server.ConnectionContext.ExecuteNonQuery(commandText);
             server.ConnectionContext.CommitTransaction();
